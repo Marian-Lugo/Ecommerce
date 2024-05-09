defcdd8ede44eae83463d5cbff561a7fd92d6601
Marian Estigarribia <mestigarribia@FUTURO.LOCAL>
2024-05-09 13:40:04 -0400

    Tienda online

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tienda/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "shop";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "PYG";
+const CLIENT_ID = "AeByAVQIkUiILxTHbjAbBCdrBkO_q3ktvJXItBEF-qA9giDSr5oafOrC0ysG2cKc-cCxcPtx9AsAzkXK";
+?>
